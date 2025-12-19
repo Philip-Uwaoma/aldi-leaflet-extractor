e30828dd4d022de8207ed20c318304aa128260cb
Philip-Uwaoma <103829082+Philip-Uwaoma@users.noreply.github.com>
2025-12-19 13:28:10 +0100

    Add start script to activate virtual environment

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Activate virtual environment
+source venv/Scripts/activate  # Windows (PowerShell) might need .\venv\Scripts\activate
+
+# Start your Python app
+python backend/app.py
